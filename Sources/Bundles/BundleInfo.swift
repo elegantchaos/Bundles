0666b5c5bee11384c3048ce59a97f374de38fb83
Sam Deane <sam@elegantchaos.com>
2020-04-01 23:25:49 +0100

    fixed linux build

---
+++ Sources/Bundles/BundleInfo.swift
@@ -0,0 +1,43 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 21/02/20.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import CollectionExtensions
+import Foundation
+import Images
+import SemanticVersion
+
+public struct BundleInfo {
+    public let name: String
+    public let id: String
+    public let executable: String
+    
+    #if canImport(UIKit) || canImport(AppKit)
+    public let icon: Image
+    #endif
+    
+    public let build: Int
+    public let version: SemanticVersion
+    public let commit: String
+    public let copyright: String
+    
+    public var versionString: String { version.asString }
+    public var fullVersionString: String { "\(version.asString) (\(build))" }
+        
+    init(for bundle: Bundle = Bundle.main) {
+        id = bundle.bundleIdentifier!
+        let info = bundle.infoDictionary!
+        name = info[stringWithKey: "CFBundleName"] ?? ""
+
+        #if canImport(UIKit) || canImport(AppKit)
+        icon = Image.imageOrBlank(named: info[stringWithKey: "CFBundleIconName"])
+        #endif
+        
+        executable = info[stringWithKey: "CFBundleExecutable"] ?? ""
+        build = info[intWithKey: "CFBundleVersion"] ?? 0
+        version = SemanticVersion(info[stringWithKey:"CFBundleShortVersionString"])
+        commit = info[stringWithKey: "Commit"] ?? ""
+        copyright = info[stringWithKey: "NSHumanReadableCopyright"] ?? ""
+    }
+}
--- Sources/Bundles/Bundles.swift
+++ Sources/Bundles/Bundles.swift
@@ -1,3 +1,48 @@
-struct Bundles {
-    var text = "Hello, World!"
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 21/01/20.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Files
+import Foundation
+
+/// Resource decoding utilities.
+/// These methods are intended for situations where you can guarantee the resource is present in the bundle, so they
+/// throw fatal errors if anything goes wrong.
+
+public extension Bundle {
+    func stringResource(named name: String, withExtension ext: String = "string", subdirectory: String? = nil) -> String {
+        guard let url = url(forResource: name, withExtension: ext, subdirectory: subdirectory) else {
+            fatalError("Missing string resource: \(name).\(ext).")
+        }
+        
+        guard let string = try? String(contentsOf: url, encoding: .utf8) else {
+            fatalError("Corrupt string resource: \(name).\(ext).")
+        }
+        return string
+    }
+
+    func dataResource(named name: String, withExtension ext: String = "data", subdirectory: String? = nil) -> Data {
+        guard let url = url(forResource: name, withExtension: ext, subdirectory: subdirectory) else {
+            fatalError("Missing data resource: \(name).\(ext).")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Corrupt data resource: \(name).\(ext).")
+        }
+        return data
+    }
+
+    func jsonResource(named name: String, withExtension ext: String = "json", subdirectory: String? = nil, options: JSONSerialization.ReadingOptions = []) -> Any {
+        let data = dataResource(named: name, withExtension: ext, subdirectory: subdirectory)
+        guard let decoded = try? JSONSerialization.jsonObject(with: data, options: options) else {
+            fatalError("Corrupt JSON resource: \(name).\(ext).")
+        }
+        return decoded
+    }
+
+    func hasFramework(named name: String) -> Bool {
+        let url = bundleURL.appendingPathComponents(["Contents", "Frameworks", "SparkleBridgeClient.framework"])
+        return FileManager.default.fileExists(atURL: url)
+    }
 }
